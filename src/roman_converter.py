d567d11deb575aec248d5171777c5e5f36acccdc
Gero2208 <ge.ortiz@alumno.um.edu.ar>
2025-04-06 14:50:36 -0300

    Implementación de la función decimal_to_roman

--- src/roman_converter.py
+++ src/roman_converter.py
@@ -1,1 +1,29 @@
 def decimalromanos(decimal):
+    conversion = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M", 4:"IV", 9:"IX", 40:"XL", 90:"XC", 400:"CD", 900:"CM"}
+
+    resultado = ""
+
+    for valor, simbolo in sorted(conversion.items(), reverse=True):
+        while decimal >= valor:
+            resultado += simbolo
+            decimal -= valor
+        
+    return resultado
+
+def main():
+
+    while True:
+        decimal = int(input("Ingrese un valor decimal:"))
+        print(decimalromanos(decimal))
+
+        while True:
+            reinicio = input("Desea ingresar otro número? (s/n)").lower()
+            if reinicio == "s":
+                break
+            if reinicio == "n":
+                return
+            else:
+                print("No ingresó una respuesta válida")
+
+if __name__ == "__main__":
+    main()
